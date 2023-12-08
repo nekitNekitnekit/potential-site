8e7b3327fb6235bd996a59853754c38cca663e5c
nekitNekitnekit <klientkv0711@gmail.com>
2023-12-09 01:54:55 +0200

    maybe work?(3)

---
+++ scripts/form.js
@@ -0,0 +1,7 @@
+$('[name="review-user-phone"]').mask('(99) 999 - 99 - 99');
+$('[name="order-user-phone"]').mask('(99) 999 - 99 - 99');
+$('[name="reg-user-phone"]').mask('(99) 999 - 99 - 99');
+
+$('[name="order-user-card"]').mask('9999 - 9999 - 9999 - 9999');
+
+// document.myForm.reset();
